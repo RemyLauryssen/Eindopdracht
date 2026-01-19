cc35e6a8068a52677baa13d0aa1d4306538e8b04
RemyLauryssen <rmp.lauryssen@gmail.com>
2026-01-19 15:29:52 +0100

    Updated webshop

--- src/pages/webshop/Webshop.jsx
+++ src/pages/webshop/Webshop.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Webshop.css";
 import LogoPlaceholder from "../../assets/logo_placeholder.svg";
+import {webshopProduct} from "./WebshopProduct.js";
 
 function Webshop() {
+
     return (
         <main>
             <div className="webshop-introduction">
@@ -13,22 +15,38 @@ function Webshop() {
             </div>
             <div className="webshop-main-container">
             <div className="product-catalog">
-                <div className="product-card">
-                    <img src={LogoPlaceholder} alt="Placeholder image"/>
-                    <h4>Placeholder artikel</h4>
-                    <p>Blablabla de lekkerste gerechten</p>
-                </div>
-                <div className="product-card">
-                    <img src={LogoPlaceholder} alt="Placeholder image"/>
-                </div>
-                <div className="product-card">
-                    <img src={LogoPlaceholder} alt="Placeholder image"/>
+
+                    {webshopProduct.map((webshopProduct) => {
+                        return (<div key={webshopProduct.id}>
+                            <div className="product-card">
+                                        <img src={webshopProduct.image}
+                                                   alt="placeholder image"
+                                        className="product-image"/>
+                                    <h4 className={webshopProduct.name}>{webshopProduct.name}</h4>
+                                <p>{webshopProduct.description}</p>
+                            </div>
+
+                        </div>)
+                    })}
+
+
+
+
+                {/*    <img src={webshopProduct[0].image} alt="Placeholder image"/>*/}
+                {/*    <h4>{webshopProduct[0].name}</h4>*/}
+                {/*    <p>{webshopProduct[0].description}</p>*/}
+                {/*</div>*/}
+                {/*<div className="product-card">*/}
+                {/*    <img src={LogoPlaceholder} alt="Placeholder image"/>*/}
+                {/*</div>*/}
+                {/*<div className="product-card">*/}
+                {/*    <img src={LogoPlaceholder} alt="Placeholder image"/>*/}
                 </div>
 
 
             </div>
 
-            </div>
+
         </main>
     );
 }
---
+++ src/pages/webshop/WebshopProduct.js
@@ -0,0 +1,32 @@
+import placeholderImage1 from "../../assets/600x400_1.svg"
+import placeholderImage2 from "../../assets/600x400_2.svg"
+
+export let webshopProduct = [{
+    id: 0,
+    name: "Taart",
+    allergies: "Gluten",
+    description: "Lorum ipsum enzovoort",
+    image: placeholderImage1
+},
+    {
+        id: 1,
+        name: "Koekjes",
+        allergies: "Suiker",
+        description: "Lorum ipsum enzovoort2",
+        image: placeholderImage2
+    },
+    {
+        id: 2,
+        name: "Gebak",
+        allergies: "Zeedieren",
+        description: "Lorum ipsum enzovoort3",
+        image: placeholderImage2
+    },
+    {
+        id: 3,
+        name: "Pizza",
+        allergies: "Kaas",
+        description: "Lorum ipsum enzovoort4",
+        image: placeholderImage2
+    }
+]
